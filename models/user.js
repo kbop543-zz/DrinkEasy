94d0204f5d67e1911fc740c52ab5133f78f2fa53
Misan Iwere <temisan.iwere@mail.utoronto.ca>
2018-01-19 23:07:56 -0500

    db added

---
+++ models/user.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+
+//connect to database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://drinkeasy:drinkeasy@ds263847.mlab.com:63847/drinkeasy');
+
+//create User model
+var userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  address1: {
+      type: String,
+      required: true
+  },
+  address2: {
+      type: String
+  },
+  city: {
+      type: String,
+      required: true
+  },
+  state: {
+      type: String,
+      required: true
+  },
+  post: {
+      type: String,
+      required: true
+  },
+  country: {
+      type: String,
+      required: true
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
