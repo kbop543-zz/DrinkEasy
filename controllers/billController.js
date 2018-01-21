b6ab6b19c8415cadfd074b91866f0dedf5cea4b3
Kyra Stephen <kyrastephen@Kyras-MacBook-Air.local>
2018-01-20 23:13:30 -0500

    added thing to bill controller

--- controllers/billController.js
+++ controllers/billController.js
@@ -1,15 +1,19 @@
-/*var Bill = require('../models/outstanding');
+var Bill = require('../models/outstanding');
 var bcrypt = require('bcrypt');
 var Menu = require('../models/menu');
 
 module.exports = function(app){
 
-    app.get('/show', function(req, res){
+    /*app.get('/show', function(req, res){
         res.render('billPreviews');
       });
 
       app.get('/outstanding', function(req, res) {
           res.render('outstanding', { title: 'DrinkEasy' });
+      });*/
+
+      app.get('/money-requests/send', function(req, res){
+          res.render('billPreviews');
       });
 
-  };*/
+  };
